0622d75ecfed63f568817a63e2b04842f45b13d3
Andreea213 <andreeariosanu06@gmail.com>
2026-05-19 01:24:29 +0300

    Update and rename ex1.c to ex1Heap.c

---
+++ ex1Heap.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int stanga = 2 * i + 1a./lab10pa;
+    int dreapta = 2 * i + 2;
+
+    if (stanga < n && arr[stanga] > arr[largest]) largest = stanga;
+    if (dreapta < n && arr[dreapta] > arr[largest]) largest = dreapta;
+
+    if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) heapify(arr, n, i);
+    for (int i = n - 1; i > 0; i--) {
+        swap(&arr[0], &arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Vector initial: ");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+
+    heapSort(arr, n);
+
+    printf("\nVector sortat: ");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+    return 0;
+}
